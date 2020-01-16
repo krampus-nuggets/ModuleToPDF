93ce7874c8e0ff061e5e22fa7051b89c3b4397e8
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2020-01-16 17:02:15 +0200

    ADD -> START Refactored => Imports

---
+++ refactor_ModuleToPDF.js
@@ -0,0 +1,6 @@
+// START - Imports
+const puppeteer = require("puppeteer");
+const uuid = require("uuid");
+const cookies = require("C:/projects/krampus-nuggets/ModuleToPDF/cookies.json");
+// END
+
