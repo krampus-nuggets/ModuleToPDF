a8a733d416e400f22a4b736878e2ed56665c2453
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2020-01-16 17:02:25 +0200

    ADD -> GLOBALS

--- refactor_ModuleToPDF.js
+++ refactor_ModuleToPDF.js
@@ -4,3 +4,8 @@ const uuid = require("uuid");
 const cookies = require("C:/projects/krampus-nuggets/ModuleToPDF/cookies.json");
 // END
 
+// START - GLOBALS
+const URL = "https://docs.microsoft.com/en-us/learn/modules/principles-cloud-computing/1-introduction";
+const units = 10;
+// END
+
