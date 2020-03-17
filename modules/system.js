fedf0df4ae3f877a10f980004bc930dea050c5c5
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-03-18 01:28:20 +0200

    ADD -> checkSystem() [banners.js]

--- modules/system.js
+++ modules/system.js
@@ -3,3 +3,24 @@
     and I don't like that so suck it ༼ つ ◕_◕ ༽つ
 */
 
+// START [OS Check]
+const checkSystem = () => {
+    let OS;
+
+    if (process.platform == "win32") {
+        OS = "windows";
+    }
+    else if (process.platform == "linux" || process.platform == "freebsd" || process.platform == "openbsd") {
+        OS = "linux";
+    }
+    else if (process.platform == "darwin") {
+        OS = "darwin";
+    } else {
+        OS = process.platform;
+        /* ¯\_(ツ)_/¯ */
+    }
+
+    return OS;
+}
+// END [OS Check]
+
